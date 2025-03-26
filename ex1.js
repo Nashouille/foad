e2fe3a7796ea8fe4a84dc7a9e60ee670f77d3942
Corentin Hautier <corentin9hautier@gmail.com>
2025-03-26 16:18:06 +0100

    toujours toujours exo pour demain

--- ex1.js
+++ ex1.js
@@ -3,9 +3,8 @@ let age = 25;
 console.log("age du client:"+age);
 
 
-let compteur = 5;
-while (compteur > 0) {
-    console.log(compteur);
-    compteur--;
+for (let i =0; i <= 20; i++) {
+    
+    if (i === 22) break;
+    console.log(i)
 }
-console.log("decollage!");

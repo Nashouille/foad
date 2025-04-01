661db7d1dbd0a70d0a5243f24e62d9332966f722
Corentin Hautier <corentin9hautier@gmail.com>
2025-04-01 10:50:36 +0200

    ..

--- doc exemple/app.js
+++ doc exemple/app.js
@@ -1,5 +1,5 @@
 let titre = document.getElementById("titre");
-
+titre.setAttribute("class", "rouge");
 console.log(titre);
 
 titre.innerHTML = "Ouais";
@@ -14,3 +14,11 @@ console.log(tousLesLiEtOl)
 
 
 let mesParagraphes = document.getElementsByTagName('p');
+
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("btn").addEventListener("click", function(){
+        let monInput = document.getElementById("nom").value;
+
+        alert(monInput);
+    });
+});

10653da29ee2a1a61253e83fd19080a256a9c7b5
Corentin Hautier <corentin9hautier@gmail.com>
2025-04-01 09:43:02 +0200

    ..

--- Exjs/app.js
+++
@@ -1,5 +0,0 @@
-let titre = document.getElementById("titre");
-
-console.log(titre);
-
-titre.innerHTML = "Ouais";
---
+++ doc exemple/app.js
@@ -0,0 +1,16 @@
+let titre = document.getElementById("titre");
+
+console.log(titre);
+
+titre.innerHTML = "Ouais";
+
+let infosOrange = document.getElementsByClassName("orange");
+console.log(infosOrange);
+
+
+let tousLesLiEtOl = document.querySelectorAll("li,ol");
+console.log(tousLesLiEtOl)
+
+
+
+let mesParagraphes = document.getElementsByTagName('p');
